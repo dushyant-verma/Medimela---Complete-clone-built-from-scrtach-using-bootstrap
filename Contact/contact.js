02ec62b35beab293f7c20295296bbd3b7f3af6b4
Dushyant Verma <167190841+dushyant-verma@users.noreply.github.com>
2024-07-06 22:48:40 +0530

    Initial commit

---
+++ Contact/contact.js
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+// [START maps_web_components_markers]
+// This example adds a map with markers, using web components.
+// [START maps_web_components_markers_initmap]
+async function initMap() {
+    console.log("Maps JavaScript API loaded.");
+  }
+  
+  window.initMap = initMap;
+  // [END maps_web_components_markers]
